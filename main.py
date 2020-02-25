0c64f9951d98e8e5e9d554f8ee60628e0960e118
Marcin Jaroszewski <mambo_zambo@o2.pl>
2020-02-25 23:59:04 +0100

    add primitive counter

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
 
 app = FastAPI()
-
+# to see what funny will come
+app.counter = 0
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get('/counter')
+def counter():
+	app.counter += 1
+	return str(app.counter)
